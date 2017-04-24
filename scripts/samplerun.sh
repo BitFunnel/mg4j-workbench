f159ffc72fd7eadc2d1f6e71a35a1a55f7e3ca15
Dan Luu <danluu@gmail.com>
2017-04-24 15:19:49 -0400

    Move status output to after point where it's generated

--- scripts/samplerun.sh
+++ scripts/samplerun.sh
@@ -4,10 +4,6 @@ java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      it.unimi.di.big.mg4j.document.TRECDocumentCollection \
      -f HtmlDocumentFactory -p encoding=iso-8859-1 ~/dev/mg4j-tmp/out2.collection ~/dev/gov2/GX000/00.txt
 
-## ???
-# cat ~/dev/mg4j-tmp/out2-text.properties
-# type d:/data/work/out2-title.properties
-
 # echo Building BitFunnel chunk files
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      org.bitfunnel.reproducibility.GenerateBitFunnelChunks \
@@ -17,6 +13,10 @@ java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      it.unimi.di.big.mg4j.tool.IndexBuilder \
       --keep-batches --downcase -S ~/dev/mg4j-tmp/out2.collection ~/dev/mg4j-tmp/out2
+
+## Index build info
+cat ~/dev/mg4j-tmp/out2-text.properties
+cat ~/dev/mg4j-tmp/out2-title.properties
 
 # echo Measuring query performance . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
