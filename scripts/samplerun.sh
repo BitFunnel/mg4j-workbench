BitFunnel/mg4j-workbench

commit 7d2c5987c38f712a9292efbce7bdd31399590f28

Fix for #14.

scripts/samplerun.sh
@@ -9,14 +9,23 @@ java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      org.bitfunnel.reproducibility.GenerateBitFunnelChunks \
      -S ~/dev/mg4j-tmp/out2.collection ~/dev/mg4j-tmp/out2.chunk
 
-# echo Building mg4j index . . .
+# echo Building mg4j index from collection . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      it.unimi.di.big.mg4j.tool.IndexBuilder \
       --keep-batches --downcase -S ~/dev/mg4j-tmp/out2.collection ~/dev/mg4j-tmp/out2
 
+# Building mg4j index from chunk . . .
+java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
+     it.unimi.di.big.mg4j.tool.IndexBuilder \
+     -o org.bitfunnel.reproducibility.ChunkDocumentSequence(~/dev/mg4j-tmp/out2.chunk) ^
+     ~/dev/mg4j-tmp/out2Chunk
+
 ## Index build info
 cat ~/dev/mg4j-tmp/out2-text.properties
+cat ~/dev/mg4j-tmp/out2Chunk-text.properties
+
 cat ~/dev/mg4j-tmp/out2-title.properties
+cat ~/dev/mg4j-tmp/out2Chunk-title.properties
 
 # echo Measuring query performance . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \

scripts/samplerun.bat
@@ -9,15 +9,24 @@ java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar ^
 echo Building BitFunnel chunks . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar ^
      org.bitfunnel.reproducibility.GenerateBitFunnelChunks ^
-      -S d:\data\work\out2.collection d:\data\work\out2.chunk
+     -S d:\data\work\out2.collection d:\data\work\out2.chunk
 
-echo Building mg4j index . . .
+echo Building mg4j index from collection . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar ^
      it.unimi.di.big.mg4j.tool.IndexBuilder ^
       --keep-batches --downcase -S d:\data\work\out2.collection d:\data\work\out2
 
+echo Building mg4j index from chunk . . .
+java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar ^
+     it.unimi.di.big.mg4j.tool.IndexBuilder ^
+     -o org.bitfunnel.reproducibility.ChunkDocumentSequence(d:\data\work\out2.chunk) ^
+     d:\data\work\out2Chunk
+
 type d:\data\work\out2-text.properties
+type d:\data\work\out2Chunk-text.properties
+
 type d:\data\work\out2-title.properties
+type d:\data\work\out2Chunk-title.properties
 
 echo Measuring query performance . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar ^

src/main/java/org/bitfunnel/reproducibility/ChunkWordReader.java
@@ -13,6 +13,8 @@ public class ChunkWordReader implements WordReader {
 
     @Override
     public boolean next(MutableString word, MutableString nonWord) throws IOException {
+        word.length(0);
+        nonWord.length(0);
         if (reader == null) {
             throw new IOException("ChunkWordReader.next(): no reader set.");
         }

commit 8cc2be0e5e456e586fffbdb0f9390165649322f3

Fix linux breakage from 7d2c5987 and other commits.

scripts/samplerun.sh
@@ -18,7 +18,7 @@ java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
 # Building mg4j index from chunk . . .
 java -cp target/mg4j-1.0-SNAPSHOT-jar-with-dependencies.jar \
      it.unimi.di.big.mg4j.tool.IndexBuilder \
-     -o org.bitfunnel.reproducibility.ChunkDocumentSequence(~/dev/mg4j-tmp/out2.chunk) ^
+     -o org.bitfunnel.reproducibility.ChunkDocumentSequence\(~/dev/mg4j-tmp/out2.chunk\) \
      ~/dev/mg4j-tmp/out2Chunk
 
 ## Index build info